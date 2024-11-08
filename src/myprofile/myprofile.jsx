0160af63e72cb8f1c89c1625d2f3021784657d6f
egriffioen <ellagriffioen@gmail.com>
2024-11-07 19:36:29 -0700

    Created view components

---
+++ src/myprofile/myprofile.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function MyProfile() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>My Profile displayed here</div>
+    </main>
+  );
+}
