3ece2b991672c4b64726b5f2915b09fb5c219d10
egriffioen <ellagriffioen@gmail.com>
2024-11-14 13:30:48 -0700

    login api functioning with error messages

--- service/index.js
+++ service/index.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.json());
 
+//app.use(express.static('public'));
+
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
@@ -14,43 +16,42 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 
 
+// CreateAuth a new user
 apiRouter.post('/auth/create', async (req, res) => {
-    const user = users[req.body.email];
-    if (user) {
-      res.status(409).send({ msg: 'Existing user' });
-    } else {
-      const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
-      users[user.email] = user;
-  
-      res.send({ token: user.token });
-    }
-  });
-  
-  // GetAuth login an existing user
-  apiRouter.post('/auth/login', async (req, res) => {
-    const user = users[req.body.email];
-    if (user) {
-      if (req.body.password === user.password) {
-        user.token = uuid.v4();
-        res.send({ token: user.token });
-        return;
-      }
-    }
-    res.status(401).send({ msg: 'Unauthorized' });
-  });
-  
-  // DeleteAuth logout a user
-  apiRouter.delete('/auth/logout', (req, res) => {
-    const user = Object.values(users).find((u) => u.token === req.body.token);
-    if (user) {
-      delete user.token;
-    }
-    res.status(204).end();
-  });
+  const user = users[req.body.email];
+  if (user) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
+    users[user.email] = user;
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    res.send({ token: user.token });
+  }
 });
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = users[req.body.email];
+  if (user) {
+    if (req.body.password === user.password) {
+      user.token = uuid.v4();
+      res.send({ token: user.token });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', (req, res) => {
+  const user = Object.values(users).find((u) => u.token === req.body.token);
+  if (user) {
+    delete user.token;
+  }
+  res.status(204).end();
+});
+
+
 
 // GetHikerStatus
 apiRouter.get('/hikerStatus', (_req, res) => {
@@ -77,5 +78,7 @@ apiRouter.post('/status', (req, res) => {
     return hikerStatus;
   }
 
-
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
 
--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
 
 import Button from 'react-bootstrap/Button';
+import { MessageDialog } from './messageDialog';
 
 export function Unauthenticated(props) {
   const [userName, setUserName] = React.useState(props.userName);
   const [password, setPassword] = React.useState('');
+  const [displayError, setDisplayError] = React.useState(null);
 
   async function loginUser() {
-    localStorage.setItem('userName', userName);
-    props.onLogin(userName);
+    loginOrCreate(`/api/auth/login`);
   }
 
   async function createUser() {
-    localStorage.setItem('userName', userName);
-    props.onLogin(userName);
+    loginOrCreate(`/api/auth/create`);
+  }
+
+  async function loginOrCreate(endpoint) {
+    const response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ email: userName, password: password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    if (response?.status === 200) {
+      localStorage.setItem('userName', userName);
+      props.onLogin(userName);
+    } else {
+      const body = await response.json();
+      setDisplayError(`⚠ Error: ${body.msg}`);
+    }
   }
 
   return (
     <><h1>Hello World!</h1>
     <div id="picture" className="picture-box"><img className="img-fluid rounded" width="500px" src="https://images.pexels.com/photos/9754/mountains-clouds-forest-fog.jpg?auto=compress&cs=tinysrgb&w=800" alt="Forest Image" /></div>
-    <form method="get" className="container loginform" action="/">
+    <div className="container loginform">
           <div className="mt-2 mb-3">Login to explore:</div>
           <div className="input-group mb-3">
               <span className="input-group-text">Username</span>
@@ -37,7 +54,8 @@ export function Unauthenticated(props) {
                   Create
               </Button>
           </div>
-      </form>
+      </div>
+      <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
       <div>
       API of Current Weather will go here
     </div>
@@ -45,6 +63,7 @@ export function Unauthenticated(props) {
         <li>Temperature: 80 degrees</li>
         <li>Weather: Partly Cloudy</li>
         <li>Heat Index: 90 degrees</li>
-    </div></>
+    </div>
+    </>
   );
 }
