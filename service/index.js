8f0aabecf5f013870b366eb63b714ba05447f337
egriffioen <ellagriffioen@gmail.com>
2024-11-15 12:27:41 -0700

    updated deploy script and added express.static in index.js

--- service/index.js
+++ service/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
