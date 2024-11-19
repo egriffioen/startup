87447ebc4526f294e85bfc16f7b948326a7ed646
egriffioen <ellagriffioen@gmail.com>
2024-11-19 16:35:24 -0700

    setup framework for database.js

--- service/database.js
+++ service/database.js
@@ -1,11 +1,16 @@
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-
-
 const client = new MongoClient(url);
-const db = client.db('rental');
+const db = client.db('startup');
+const userCollection = db.collection('user');
+const hikerStatusCollection = db.collection('hikerStatus');
+const hikerLogCollection = db.collection('hikerLog');
 
+//Test the connection
 (async function testConnection() {
   await client.connect();
   await db.command({ ping: 1 });
