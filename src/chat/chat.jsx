4eef709b7c21e5b064c48b0bbba91b7eec7a577e
egriffioen <ellagriffioen@gmail.com>
2024-12-03 23:24:45 -0700

    Framework for html added for live chat

--- src/chat/chat.jsx
+++ src/chat/chat.jsx
@@ -37,33 +37,20 @@ export function Chat() {
 
   return (
     <main className="container-fluid bg-success text-center mt-5 pt-5 pb-3">
-      <h2>CHAT GOES HERE (WebSocket Placeholder)</h2>
-      <div className="container-fluid pt-4 row d-flex justify-content-center align-items-start">
-        {/* Example chats */}
-        <div className="container row col-md-5 chatborder d-flex justify-content-center align-items-center pb-4">
-          <span className="chatheader">Chatting with Peter</span>
-          <p className="container messageborder">
-            Peter: I heard about your hike to the Y! How was it?
-          </p>
-          <p className="container messageborder">
-            You: It was awesome! Beautiful views and it was so good to do that hike with my sister.
-          </p>
-          <p className="container messageborder">
-            Peter: That sounds fun! It's definitely a steep climb, but it's so satisfying when you get to the top
-          </p>
-        </div>
-        <div className="container row col-md-5 chatborder d-flex justify-content-center align-items-center">
-          <span className="chatheader">Chatting with Emmet</span>
-          <p className="container messageborder"> 
-            You: Hey Emmet! I'm new here to Provo, and I'm looking to hike more. What has been one of your most 
-            memorable hikes?
-          </p>
-          <p className="container messageborder">
-            Emmet: Welcome to Provo! Where did you move from? I love hiking here, the mountains are just breath taking.
-            I think one of the most memorable hikes was when my friends and I hiked Mount Timpanogos.
-          </p>
-        </div>
+      <h2>Live Chat</h2>
+      <div class="name">
+        <fieldset id="name-controls">
+          <legend>My Name</legend>
+          <input id="my-name" type="text" />
+        </fieldset>
       </div>
+
+      <fieldset id="chat-controls" disabled>
+        <legend>Chat</legend>
+        <input id="new-msg" type="text" />
+        <button onclick="sendMessage()">Send</button>
+      </fieldset>
+      <div id="chat-text"></div>
 
       <h2>Explorer Hiking Status</h2>
       <table className="table table-bordered text-white container hikerscoretable">
